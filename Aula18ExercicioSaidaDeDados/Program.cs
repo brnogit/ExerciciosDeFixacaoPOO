221b793ad00c4b9a8cc45c3c695d1a1c9032cd28
Bruno Mota <bmotadev@gmail.com>
2022-12-09 16:55:12 -0300

    Add project files.

---
+++ Aula18ExercicioSaidaDeDados/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5590;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+        }
+    }
+}
