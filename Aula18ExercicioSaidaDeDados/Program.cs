eb6e75f6f7dbc5306be2dd55d10699339dada937
Bruno Mota <bmotadev@gmail.com>
2022-12-09 17:12:30 -0300

    exercicio de fixacao finalizado

--- Aula18ExercicioSaidaDeDados/Program.cs
+++ Aula18ExercicioSaidaDeDados/Program.cs
@@ -17,6 +17,16 @@ namespace SaidaDeDados
             double preco1 = 2100.0;
             double preco2 = 650.50;
             double medida = 53.234567;
+
+            Console.WriteLine("Produtos: ");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1:f2}");
+            Console.WriteLine($"{produto2}m cujo preço é $ {preco2:f2}");
+            Console.WriteLine();
+            Console.WriteLine($"{idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine();
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
         }
     }
 }
