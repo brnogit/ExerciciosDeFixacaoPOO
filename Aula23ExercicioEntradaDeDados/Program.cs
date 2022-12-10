a8a731b8717b90192d816d1a2bb9f0a1ff23e2c4
Bruno Mota <bmotadev@gmail.com>
2022-12-10 19:07:18 -0300

    Aula 23 - Exercicio Entrada de Dados

---
+++ Aula23ExercicioEntradaDeDados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeCompleto, ultimoNome;
+            int quartos, idade;
+            double precoDoProduto, altura;
+
+            Console.WriteLine("Entre com seu nome completo: ");
+            nomeCompleto = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Entre com o preço de um produto: ");
+            precoDoProduto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Entre com seu último nome, idade e altura(na mesma linha):");
+            string[] vet = Console.ReadLine().Split(' ');
+            Console.WriteLine();
+            ultimoNome = vet[0];
+            idade = int.Parse(vet[1]);
+            altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.WriteLine($"{nomeCompleto}");
+            Console.WriteLine($"{quartos}");
+            Console.WriteLine($"{precoDoProduto.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"{ultimoNome}");
+            Console.WriteLine($"{idade}");
+            Console.WriteLine($"{altura.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
