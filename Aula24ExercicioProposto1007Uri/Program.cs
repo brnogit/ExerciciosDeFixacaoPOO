9d2cb4b72d8cb4060b0abe4c6886e7307044678e
Bruno Mota <bmotadev@gmail.com>
2022-12-10 19:32:07 -0300

    Aula 24 - Exercicio Proposto 1007 URI

---
+++ Aula24ExercicioProposto1007Uri/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA, valorB, valorC, valorD, diferenca;
+
+            valorA = int.Parse(Console.ReadLine());
+            valorB = int.Parse(Console.ReadLine());
+            valorC = int.Parse(Console.ReadLine());
+            valorD = int.Parse(Console.ReadLine());
+
+            diferenca = ((valorA * valorB) - (valorC * valorD));
+
+            Console.WriteLine($"DIFERENCA = {diferenca}");
+        }
+    }
+}
