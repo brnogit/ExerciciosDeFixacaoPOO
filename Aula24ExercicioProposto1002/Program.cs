2cc2c68e64f33fcb5352e3983858d6eb9302e9e1
Bruno Mota <bmotadev@gmail.com>
2022-12-10 19:27:37 -0300

    Aula 24 - Exercicio Proposto 1002

---
+++ Aula24ExercicioProposto1002/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio;
+            double pi = 3.14159;
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double formulaArea = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine($"A = {formulaArea.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
