9022ba4a597c224f48ed0997d42bb5f4b0057197
Bruno Mota <bmotadev@gmail.com>
2022-12-10 19:20:52 -0300

    Aula 24 - Exercicio Proposto 1003 do URI

---
+++ Aula24ExercicioProposto1003Uri/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA, valorB, soma;
+            valorA = int.Parse(Console.ReadLine());
+            valorB = int.Parse(Console.ReadLine());
+
+            soma = valorA + valorB;
+
+            Console.WriteLine($"SOMA = {soma}");
+
+        }
+    }
+}
