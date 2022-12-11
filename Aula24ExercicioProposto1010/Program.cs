275abbc2baa4f121b0d29c5b82572a5e6195725b
Bruno Mota <bmotadev@gmail.com>
2022-12-11 12:51:10 -0300

    Aula 24 - Exercicio Proposto 1010 URI

---
+++ Aula24ExercicioProposto1010/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigoPeca1, numeroDePeca1, codigoPeca2, numeroDePeca2;
+            double valorPeca1, valorPeca2, valorTotal;
+
+            string[] vet = Console.ReadLine().Split();
+            codigoPeca1 = int.Parse(vet[0]);
+            numeroDePeca1 = int.Parse(vet[1]);
+            valorPeca1 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            string[] vet1 = Console.ReadLine().Split();
+            codigoPeca2 = int.Parse(vet1[0]);
+            numeroDePeca2 = int.Parse(vet1[1]);
+            valorPeca2 = double.Parse(vet1[2], CultureInfo.InvariantCulture);
+
+            valorTotal = (numeroDePeca1 * valorPeca1) + (numeroDePeca2 * valorPeca2);
+
+            Console.WriteLine($"VALOR A PAGAR: R$ {valorTotal.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
