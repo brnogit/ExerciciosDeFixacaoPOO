6d3ed9a2fb4fa16a1cdff1378a77bb23ccbadbbe
Bruno Mota <bmotadev@gmail.com>
2022-12-14 17:10:41 -0300

    Aula 29 - Exercicio Proposto 01

---
+++ Aula29ExercicioProposto01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero >= 0 )
+            {
+                Console.WriteLine("NAO NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+        }
+    }
+}
