0dd7504753eab39e793b98ecf26762592fb061d2
Bruno Mota <bmotadev@gmail.com>
2022-12-14 17:23:51 -0300

    Aula 29 - Exercicio Proposto 02

---
+++ Aula29ExercicioProposto02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            numero = int.Parse(Console.ReadLine());
+            if(numero % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
