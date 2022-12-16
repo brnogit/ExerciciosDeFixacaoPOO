7f2311d1684908469d53caa76ea3614d6440be4a
Bruno Mota <bmotadev@gmail.com>
2022-12-16 16:05:45 -0300

    Aula 29 Exercicio Proposto 08 Uri 1051

---
+++ Aula29ExercicioProposto08Uri1051/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorDoImposto8, valorDoImposto18, valorDoImposto28, totalImposto;
+            double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (renda <= 2000.00)
+            {
+                Console.WriteLine("Isento");
+            }
+            else if (renda >= 2000.01 && renda <= 3000.00)
+            {
+
+                totalImposto = (renda - 2000) * 0.08;
+                Console.WriteLine($"R$ {totalImposto:F2}");
+            }
+            else if (renda >= 3000.01 && renda <= 4500.00)
+            {
+
+                valorDoImposto8 = 1000.00 * 0.08;
+                renda = renda - 3000.00;
+                valorDoImposto18 = renda * 0.18;
+                totalImposto = valorDoImposto8 + valorDoImposto18;
+
+                Console.WriteLine($"R$ {totalImposto:F2}");
+            }
+            else
+            {
+                valorDoImposto8 = 1000.00 * 0.08;
+                valorDoImposto18 = 1500.00 * 0.18;
+                renda = renda - 4500.00;
+                valorDoImposto28 = renda * 0.28;
+                totalImposto = valorDoImposto8 + valorDoImposto18 + valorDoImposto28;
+
+                Console.WriteLine($"R$ {totalImposto:F2}");
+
+            }
+        }
+    }
+}
