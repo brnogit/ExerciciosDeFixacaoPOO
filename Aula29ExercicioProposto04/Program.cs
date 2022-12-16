c79d6a64d36b5d4d4de3db707114e8dc1fdd084c
Bruno Mota <bmotadev@gmail.com>
2022-12-16 14:29:16 -0300

    Aula 29 - Exercicio Proposto 04

---
+++ Aula29ExercicioProposto04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInicial, horaFinal, duracaoDoJogo;
+            string[] vet = Console.ReadLine().Split(' ');
+            horaInicial = int.Parse(vet[0]);
+            horaFinal = int.Parse(vet[1]);
+
+            if(horaInicial >= horaFinal)
+            {
+                duracaoDoJogo = 24 - (horaInicial - horaFinal);
+            }
+            else
+            {
+                duracaoDoJogo = horaFinal - horaInicial;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {duracaoDoJogo} HORA(S)");
+        }
+    }
+}
