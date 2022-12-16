3f7245bad9582276ef6fe6c5cde521079c44c601
Bruno Mota <bmotadev@gmail.com>
2022-12-16 14:22:33 -0300

    Aula 29 Exercicio Proposto 03 Concluido

--- Aula29ExercicioProposto03/Program.cs
+++ Aula29ExercicioProposto03/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            string[] vet = Console.ReadLine().Split();
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+
+            if(a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("São Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são multiplos");
+            }
+        }
+    }
+}
