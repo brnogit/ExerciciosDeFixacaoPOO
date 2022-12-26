bdcb847018cf7fc3af02478d1111f5e82b52ae82
Bruno Mota <bmotadev@gmail.com>
2022-12-26 19:07:49 -0300

    Aula 33 - Exercicio Proposto 02 URI 1115

---
+++ Aula33ExercicioProposto02Uri1115/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coodernadaX = 0, coodernadaY = 0;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            coodernadaX = int.Parse(vet[0]);
+            coodernadaY = int.Parse(vet[1]);
+
+            while(coodernadaX != 0 && coodernadaY != 0)
+            {
+                if(coodernadaX > 0 && coodernadaY > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if(coodernadaX < 0 && coodernadaY > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                else if(coodernadaX < 0 && coodernadaY < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                else if(coodernadaX > 0 && coodernadaY < 0)
+                {
+                    Console.WriteLine("quarto");
+                }
+
+                vet = Console.ReadLine().Split(' ');
+                coodernadaX = int.Parse(vet[0]);
+                coodernadaY = int.Parse(vet[1]);
+            }
+        }
+    }
+}
