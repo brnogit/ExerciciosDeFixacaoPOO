767963061a3b69891a5f8e518377b60c06c267e8
Bruno Mota <bmotadev@gmail.com>
2022-12-27 20:43:39 -0300

    Aula 33 - Exercicio Proposto 03 URI 1134

---
+++ Aula33ExercicioProposto03Uri1134/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0, gasolina = 0, diesel = 0, codigo = 0;
+
+            codigo = int.Parse(Console.ReadLine());
+
+            while(codigo != 4)
+            {
+                switch (codigo)
+                {
+                    case 1:
+                        alcool++;
+                        break;
+
+                    case 2:
+                        gasolina++;
+                        break;
+
+                    case 3:
+                        diesel++;
+                        break;
+
+                }
+                codigo = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("MUITO OBRIGADO!");
+            Console.WriteLine($"Alcool: {alcool}");
+            Console.WriteLine($"Gasolina: {gasolina}");
+            Console.WriteLine($"Diesel: {diesel}");
+        }
+    }
+}
