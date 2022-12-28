1aa4bda98ece3bda62c91b95b3e099c00f5899da
Bruno Mota <bmotadev@gmail.com>
2022-12-28 20:19:12 -0300

    Aula 35 Exercicio Proposto 04 Uri 1116

---
+++ Aula35ExercicioProposto04Uri1116/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorN = 0, numeroUm = 0, numeroDois = 0 ;
+            double divisao = 0.0;
+            valorN = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < valorN; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                numeroUm = int.Parse(vet[0], CultureInfo.InvariantCulture);
+                numeroDois = int.Parse(vet[1], CultureInfo.InvariantCulture);
+
+                
+                if(numeroDois == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    divisao = (double)numeroUm / numeroDois;
+                    Console.WriteLine(divisao.ToString("F1"), CultureInfo.InvariantCulture);
+                }
+
+            }
+        }
+    }
+}
