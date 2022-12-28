002438b1d49430ad2006d6290fe33778853782e3
Bruno Mota <bmotadev@gmail.com>
2022-12-28 19:31:09 -0300

    Aula 35 - Exercicio Proposto 01 - URI 1067 -Concluido

---
+++ Aula35ExercicioProposto01Uri1067/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorX = 0;
+
+            valorX = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < valorX; i++)
+            {
+                if(i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
