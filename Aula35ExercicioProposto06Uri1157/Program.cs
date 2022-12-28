be3d8c70e1957b51182beb1931e84e7bf38d67b2
Bruno Mota <bmotadev@gmail.com>
2022-12-28 20:58:07 -0300

    Aula 35 Exercicio Proposto 06 Uri 1157

---
+++ Aula35ExercicioProposto06Uri1157/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorN;
+            valorN = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i  <= valorN; i++)
+            {
+                if(valorN % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
