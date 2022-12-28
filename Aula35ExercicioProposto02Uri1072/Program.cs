2164c794ee005364338606f101142108212be25d
Bruno Mota <bmotadev@gmail.com>
2022-12-28 19:37:48 -0300

    Aula 35 - Exercicio Proposto 02 - Uri 1072 - Concluido

---
+++ Aula35ExercicioProposto02Uri1072/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorN = 0, valorX = 0, valorIn = 0, valorOut = 0;
+
+            valorN = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < valorN; i++)
+            {
+                valorX = int.Parse(Console.ReadLine());
+
+                if(valorX >= 10 && valorX <= 20)
+                {
+                    valorIn++;
+                }
+                else
+                {
+                    valorOut++;
+                }
+            }
+
+            Console.WriteLine($"{valorIn} in");
+            Console.WriteLine($"{valorOut} out");
+        }
+    }
+}
