87977deedcdb6d925fb68c65a87e719db5be0092
Bruno Mota <bmotadev@gmail.com>
2022-12-28 20:52:59 -0300

    Aula 35 Exercicio Proposto 05 Uri 1153 - Concluido

---
+++ Aula35Exercicioproposto05Uri1153/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorN = 0, fatorialN = 1;
+            valorN = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= valorN; i++)
+            {
+                fatorialN *= i;
+                
+            }
+
+            Console.WriteLine(fatorialN);
+        }
+    }
+}
