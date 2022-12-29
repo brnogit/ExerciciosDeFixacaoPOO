dca74a86031d43c933a4f60d6f3b927ced24f8e1
Bruno Mota <bmotadev@gmail.com>
2022-12-29 16:24:05 -0300

    Aula 40 Exercicio Proposto 01

---
+++ Aula40ExercicioProposto01/Program.cs
@@ -0,0 +1,29 @@
+﻿using Aula40ExercicioProposto01;
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            Pessoa pessoa2 = new Pessoa();
+
+            pessoa1.Nome = Console.ReadLine();
+            pessoa1.Idade = int.Parse(Console.ReadLine());
+
+            pessoa2.Nome = Console.ReadLine();
+            pessoa2.Idade = int.Parse(Console.ReadLine());
+
+            if(pessoa1.Idade > pessoa2.Idade)
+            {
+                Console.WriteLine(pessoa1.Nome);
+            }
+            else
+            {
+                Console.WriteLine(pessoa2.Nome);
+            }
+        }
+    }
+}
