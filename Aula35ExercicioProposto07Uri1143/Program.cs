b7bd5443429215a01fdf22d2669a6f31e03d756f
Bruno Mota <bmotadev@gmail.com>
2022-12-29 12:20:13 -0300

    Aula 35 Exercicio Proposto 07
    - URI 1143
    - Concluído

---
+++ Aula35ExercicioProposto07Uri1143/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroN = 0, quadrado = 0, cubo = 0;
+            numeroN = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= numeroN; i++)
+            {
+                quadrado = i * i;
+                cubo = quadrado * i;
+
+                Console.WriteLine($"{i} {quadrado} {cubo}");
+                
+            }
+        }
+    }
+}
