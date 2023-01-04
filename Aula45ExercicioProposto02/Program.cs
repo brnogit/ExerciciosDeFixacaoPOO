e36a474a3260509b8736e3d2d10feecaf7b98bff
Bruno Mota <bmotadev@gmail.com>
2023-01-04 13:20:35 -0300

    Aula 45 Exercicio Proposto 02 - Concluido - Calcula Salario Liquido - Calcula Aumento de Salario

---
+++ Aula45ExercicioProposto02/Program.cs
@@ -0,0 +1,30 @@
+﻿using Aula45ExercicioProposto02;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine($"Funcionário: {funcionario.Nome}, $ {funcionario.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine();
+            Console.Write($"Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagem);
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {funcionario.Nome}, $ {funcionario.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
