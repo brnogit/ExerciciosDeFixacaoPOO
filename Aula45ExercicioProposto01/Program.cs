73ef8f3665f6764dc2808bc74ddf779abb6a4487
Bruno Mota <bmotadev@gmail.com>
2023-01-04 12:16:29 -0300

    Adiciona o metodo para calcular a area

--- Aula45ExercicioProposto01/Program.cs
+++ Aula45ExercicioProposto01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Aula45ExercicioProposto01;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retângulo:");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"AREA = {retangulo.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Aula45ExercicioProposto01/Retangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula45ExercicioProposto01
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+    }
+}
