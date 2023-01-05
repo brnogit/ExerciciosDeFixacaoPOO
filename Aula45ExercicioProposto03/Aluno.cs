d25567ae29f0396bea0a49a7a8f11845c345c7e2
Bruno Mota <bmotadev@gmail.com>
2023-01-04 22:01:32 -0300

    Aula 45 Exercicio Proposto 03 - Concluido - Calculo das Notas - Status Aprovado ou Reprovado - Pontos que faltaram

---
+++ Aula45ExercicioProposto03/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula45ExercicioProposto03
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public string Status()
+        {
+            if(NotaFinal() >= 60)
+            {
+                return "APROVADO";
+            }
+            else
+            {
+                return "REPROVADO";
+            }
+        }
+
+        public double Pontos()
+        {
+                return 60 - NotaFinal();
+
+        }
+    }
+}
---
+++ Aula45ExercicioProposto03/Program.cs
@@ -0,0 +1,29 @@
+﻿using Aula45ExercicioProposto03;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"NOTA FINAL = {aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"{aluno.Status()}");
+
+            if (aluno.Status() == "REPROVADO")
+            {
+                Console.WriteLine($"FALTARAM {aluno.Pontos().ToString("F2", CultureInfo.InvariantCulture)} PONTOS");
+            }
+
+        }
+    }
+}
